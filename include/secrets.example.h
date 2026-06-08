95398545cd956e619053dbbbc849bd49fbde5e2e
Wayne Roberts <fivepanelhat@gmail.com>
2026-06-08 19:03:08 +1200

    chore: document Weaver MQTT config template in secrets.example.h

--- include/secrets.example.h
+++ include/secrets.example.h
@@ -8,3 +8,8 @@ const char* WIFI_SSID = "YOUR_LOCAL_SSID";
 const char* WIFI_PASSWORD = "YOUR_WIFI_PASSWORD";
 const char* HARDWARE_SECRET = "GENERATE_DEVICE_SECRET_HERE";
 const char* PORTAL_IP = "192.168.x.x";
+
+// --- New Weaver Config ---
+const char* WEAVER_MQTT_IP = "192.168.x.x";
+const int WEAVER_MQTT_PORT = 1883;
+
